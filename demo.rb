9bd712b6618c4d084d7fd5757690a80c061ed2fa
Ali Saidik <saida130.210@gmail.com>
2019-10-18 11:28:23 +0000

    Automatically backed up by Learn

---
+++ demo.rb
@@ -0,0 +1,8 @@
+count = 0 
+n = 2
+
+while count < 4 do
+  puts "yes"
+  count += 1
+  break if count <= n
+end
---
+++ greeting.rb
@@ -0,0 +1,15 @@
+=begin name = "tokio"
+language = "spanish"
+
+def hola(name, language)
+  puts "hello im #{name} and i speak #{language}"
+end
+
+hola("tokio", "spanish")
+=end
+
+def default_arg(number = 600)
+  puts "ronaldo won #{number} tropheis"
+end
+
+default_arg(700)
